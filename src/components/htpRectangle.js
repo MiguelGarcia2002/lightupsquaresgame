58e31a75328cc0911b4e8480c8ece283a8e372e1
Miguel Garcia <112121391+MiguelGarcia2002@users.noreply.github.com>
2023-06-14 00:12:17 -0400

    Added component for the howToPlay page

---
+++ src/components/htpRectangle.js
@@ -0,0 +1,17 @@
+import '../Styles/HowToPlay.sass'
+
+function HtpRectangle(){
+    return(
+        <div className='textContainer'>
+            <div className='textHalf'>
+                <h2 className='header'> Start of the game </h2>
+                <p className='paragraph'> Palkia (Japanese: パルキア Palkia) is a dual-type Water/Dragon Legendary Pokémon introduced in Generation IV. While it is not known to evolve into or from any other Pokémon, Palkia transforms into a second form, called its Origin Forme, while holding a Lustrous Globe</p>
+            </div>
+
+            <div className='squarehalf'>
+                <div className='square1'></div>
+            </div>
+        </div>
+    )
+}
+export default HtpRectangle
--- src/pages/Howtoplay.js
+++ src/pages/Howtoplay.js
@@ -1,11 +1,34 @@
 import '../Styles/HowToPlay.sass'
+import HtpRectangle from '../components/htpRectangle.js'
 
 function Howtoplay(){
     return (
-        <div classname='howToPlayPage'>
+        <div className='howToPlayPage'>
 
-            <p classname='title'>How To Play </p>
+            <h1 className='title'>How To Play </h1>
+            <div className='redCard'> </div>
+            
+            <div className='colorCard'> </div>
 
+            <div className='backgroundContainer'>
+                
+                
+                <HtpRectangle></HtpRectangle>
+                <HtpRectangle></HtpRectangle>
+                <HtpRectangle></HtpRectangle>
+
+                <div className='square1'></div>
+                <div className='square2'></div>
+                <div className='square3'></div>
+
+                <HtpRectangle></HtpRectangle>
+                
+            </div>
+
+            
+            
+
+            
         </div>
        
     )
